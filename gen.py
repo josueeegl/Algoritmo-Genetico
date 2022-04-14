0c42c82b8265a34819429ebe31722a7488c4b852
josueeegl <josuegl12@outlook.com>
2022-04-13 23:36:29 -0600

    v1

--- gen.py
+++ gen.py
@@ -1,30 +1,13 @@
 import numpy as np
 import random
-
-class DNA():
-    def __init__(self, fitness, mutacion_rate, nIndividuos, nSelection, nGeneraciones, imprimir = True):
-        self.fitness = fitness
-        self.mutacion_rate = mutacion_rate
-        self.nIndividuos = nIndividuos
-        self.nSelection = nSelection
-        self.nGeneraciones = nGeneraciones
-    
-
-    """ esta funcion es para crear un individuo """
-    def crear_individuos(self, min = 0, max = 9): 
-        individuos = [np.random.randint(min, max) for i in range(len(self.fitness))]
-        return individuos
+import functions.individuos as indiv
 
 
-    """ Se crea la poblacion de 15 individuos """
-    def crear_poblacion(self):
-        poblacion = [self.crear_individuos() for i in range(self.nIndividuos)]
-        print(poblacion)
 
 def main():
-    fitness = [1,0,0,1,0,1.1]
-    model = DNA(fitness=fitness, mutacion_rate = 0.02, nIndividuos = 15, nSelection = 5, nGeneraciones=50, imprimir=False)
-    model.crear_poblacion()
+    target = [2,2,2,2,2,2,2]
+    model = indiv.DNA(target=target, mutacion_rate = 0.2, nIndividuos = 25, nSelection = 5, nGeneraciones=50, imprimir=True)
+    model.run_AlgoGen()
 
 if __name__ == '__main__':
     main()
